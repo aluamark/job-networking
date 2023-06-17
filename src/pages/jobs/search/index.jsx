827369e77f3ba10fe5420690818a878ef776aeba
aluamark <aluamark@gmail.com>
2023-06-17 16:58:24 +0800

    -added job location type filter

--- src/pages/jobs/search/index.jsx
+++ src/pages/jobs/search/index.jsx
@@ -22,18 +22,18 @@ const Search = () => {
 	const [jobs, setJobs] = useState([]);
 
 	const [jobType, setJobType] = useState({
-		fullTime: false,
-		partTime: false,
-		contract: false,
-		temporary: false,
-		internship: false,
-		volunteer: false,
+		FT: false,
+		PT: false,
+		C: false,
+		T: false,
+		I: false,
+		V: false,
 	});
 
 	const [locationType, setLocationType] = useState({
-		onSite: false,
-		hybrid: false,
-		remote: false,
+		OS: false,
+		H: false,
+		R: false,
 	});
 
 	const handleCheckboxChange = (key) => {
@@ -53,43 +53,59 @@ const Search = () => {
 	};
 
 	const handleShowResults = () => {
-		const filters = [];
+		const JTfilters = [];
+		const LTfilters = [];
 
-		if (jobType.fullTime) {
-			filters.push("FT");
+		if (jobType.FT) {
+			JTfilters.push("FT");
+		}
+		if (jobType.PT) {
+			JTfilters.push("PT");
+		}
+		if (jobType.C) {
+			JTfilters.push("C");
+		}
+		if (jobType.T) {
+			JTfilters.push("T");
+		}
+		if (jobType.I) {
+			JTfilters.push("I");
+		}
+		if (jobType.V) {
+			JTfilters.push("V");
 		}
 
-		if (jobType.partTime) {
-			filters.push("PT");
+		if (locationType.OS) {
+			LTfilters.push("OS");
 		}
-		if (jobType.contract) {
-			filters.push("C");
+		if (locationType.R) {
+			LTfilters.push("R");
 		}
-		if (jobType.temporary) {
-			filters.push("T");
-		}
-
-		if (jobType.internship) {
-			filters.push("I");
-		}
-		if (jobType.volunteer) {
-			filters.push("V");
+		if (locationType.H) {
+			LTfilters.push("H");
 		}
 
-		if (
-			!jobType.fullTime &&
-			!jobType.partTime &&
-			!jobType.contract &&
-			!jobType.temporary &&
-			!jobType.internship &&
-			!jobType.volunteer
-		) {
+		const hasTrueValue = (obj) => Object.values(obj).includes(true);
+
+		if (!hasTrueValue(jobType) && !hasTrueValue(locationType)) {
 			router.push(`/jobs/search?keywords=${encodeURIComponent(keywords)}`);
+		} else if (hasTrueValue(jobType) && !hasTrueValue(locationType)) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&JT=${JTfilters.join("-")}`
+			);
+		} else if (!hasTrueValue(jobType) && hasTrueValue(locationType)) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&LT=${LTfilters.join("-")}`
+			);
 		} else {
 			router.push(
 				`/jobs/search?keywords=${encodeURIComponent(
 					keywords
-				)}&JT=${filters.join("-")}`
+				)}&JT=${JTfilters.join("-")}&LT=${LTfilters.join("-")}`
 			);
 		}
 	};
@@ -109,61 +125,36 @@ const Search = () => {
 				}
 			}
 
-			if (JT) {
-				let filteredJobs = [];
+			if (JT && !LT) {
 				const jobTypeQuery = JT.split("-");
+				const filteredJobs = [];
 				const filteredJobType = {
-					fullTime: false,
-					partTime: false,
-					contract: false,
-					temporary: false,
-					internship: false,
-					volunteer: false,
+					FT: false,
+					PT: false,
+					C: false,
+					T: false,
+					I: false,
+					V: false,
 				};
 
-				if (jobTypeQuery.includes("FT")) {
-					const fullTimeJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Full-time"
-					);
-					filteredJobs = [...filteredJobs, ...fullTimeJobs];
-					filteredJobType.fullTime = true;
-				}
-				if (jobTypeQuery.includes("PT")) {
-					const partTimeJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Part-time"
-					);
-					filteredJobs = [...filteredJobs, ...partTimeJobs];
-					filteredJobType.partTime = true;
-				}
-				if (jobTypeQuery.includes("C")) {
-					const contractJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Contract"
-					);
-					filteredJobs = [...filteredJobs, ...contractJobs];
-					filteredJobType.contract = true;
-				}
-				if (jobTypeQuery.includes("T")) {
-					const temporaryJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Temporary"
-					);
-					filteredJobs = [...filteredJobs, ...temporaryJobs];
-					filteredJobType.temporary = true;
-				}
+				const jobTypeMapping = {
+					FT: "Full-time",
+					PT: "Part-time",
+					C: "Contract",
+					T: "Temporary",
+					I: "Internship",
+					V: "Volunteer",
+				};
 
-				if (jobTypeQuery.includes("I")) {
-					const internshipJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Internship"
-					);
-					filteredJobs = [...filteredJobs, ...internshipJobs];
-					filteredJobType.internship = true;
-				}
-				if (jobTypeQuery.includes("V")) {
-					const volunteerJobs = uniqueJobs.filter(
-						(job) => job.employmentType === "Volunteer"
-					);
-					filteredJobs = [...filteredJobs, ...volunteerJobs];
-					filteredJobType.volunteer = true;
-				}
+				jobTypeQuery.forEach((type) => {
+					if (jobTypeMapping[type]) {
+						const jobsOfType = uniqueJobs.filter(
+							(job) => job.employmentType === jobTypeMapping[type]
+						);
+						filteredJobs.push(...jobsOfType);
+						filteredJobType[type] = true;
+					}
+				});
 
 				setJobType(filteredJobType);
 				setJobs(
@@ -171,8 +162,143 @@ const Search = () => {
 						(a, b) => new Date(a.createdAt) - new Date(b.createdAt)
 					)
 				);
-				setSelectedJob(jobs[0]);
+
+				if (filteredJobs.length !== 0) {
+					setSelectedJob(filteredJobs[0]);
+				} else {
+					setSelectedJob(null);
+				}
+			} else if (!JT && LT) {
+				const locationTypeQuery = LT.split("-");
+				const filteredJobs = [];
+				const filteredLocationType = {
+					OS: false,
+					H: false,
+					R: false,
+				};
+
+				const locationTypeMapping = {
+					OS: "On-site",
+					R: "Remote",
+					H: "Hybrid",
+				};
+
+				locationTypeQuery.forEach((type) => {
+					if (locationTypeMapping[type]) {
+						const jobsOfType = uniqueJobs.filter(
+							(job) => job.locationType === locationTypeMapping[type]
+						);
+						filteredJobs.push(...jobsOfType);
+						filteredLocationType[type] = true;
+					}
+				});
+
+				setLocationType(filteredLocationType);
+				setJobs(
+					filteredJobs.sort(
+						(a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+					)
+				);
+
+				if (filteredJobs.length !== 0) {
+					setSelectedJob(filteredJobs[0]);
+				} else {
+					setSelectedJob(null);
+				}
+			} else if (JT && LT) {
+				const jobTypeQuery = JT.split("-");
+				const locationTypeQuery = LT.split("-");
+
+				const jobTypeMapping = {
+					"Full-time": "FT",
+					"Part-time": "PT",
+					Contract: "C",
+					Temporary: "T",
+					Internship: "I",
+					Volunteer: "V",
+				};
+
+				const locationTypeMapping = {
+					"On-site": "OS",
+					Remote: "R",
+					Hybrid: "H",
+				};
+
+				const filteredJobs = uniqueJobs.filter((job) => {
+					return (
+						jobTypeQuery.includes(jobTypeMapping[job.employmentType]) &&
+						locationTypeQuery.includes(locationTypeMapping[job.locationType])
+					);
+				});
+
+				const filteredJobType = {
+					FT: false,
+					PT: false,
+					C: false,
+					T: false,
+					I: false,
+					V: false,
+				};
+				const filteredLocationType = {
+					OS: false,
+					H: false,
+					R: false,
+				};
+
+				const jobTypes = {
+					FT: "Full-time",
+					PT: "Part-time",
+					C: "Contract",
+					T: "Temporary",
+					I: "Internship",
+					V: "Volunteer",
+				};
+
+				const locationTypes = {
+					OS: "On-site",
+					R: "Remote",
+					H: "Hybrid",
+				};
+
+				jobTypeQuery.forEach((type) => {
+					if (jobTypes[type]) {
+						filteredJobType[type] = true;
+					}
+				});
+
+				locationTypeQuery.forEach((type) => {
+					if (locationTypes[type]) {
+						filteredLocationType[type] = true;
+					}
+				});
+
+				setJobType(filteredJobType);
+				setLocationType(filteredLocationType);
+				setJobs(
+					filteredJobs.sort(
+						(a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+					)
+				);
+
+				if (filteredJobs.length !== 0) {
+					setSelectedJob(filteredJobs[0]);
+				} else {
+					setSelectedJob(null);
+				}
 			} else {
+				setJobType({
+					FT: false,
+					PT: false,
+					C: false,
+					T: false,
+					I: false,
+					V: false,
+				});
+				setLocationType({
+					OS: false,
+					H: false,
+					R: false,
+				});
 				setJobs(
 					uniqueJobs.sort(
 						(a, b) => new Date(a.createdAt) - new Date(b.createdAt)
@@ -181,17 +307,29 @@ const Search = () => {
 				setSelectedJob(jobs[0]);
 			}
 		}
-	}, [search.data, JT]);
+	}, [search.data, JT, LT]);
 
 	const handleJobClick = (job) => {
 		setSelectedJob(job);
 		setJobViewModal(true);
 
-		if (JT) {
+		if (JT && !LT) {
 			router.push(
 				`/jobs/search?keywords=${encodeURIComponent(
 					keywords
 				)}&JT=${JT}&currentJobId=${job._id}`
+			);
+		} else if (!JT && LT) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&LT=${LT}&currentJobId=${job._id}`
+			);
+		} else if (JT && LT) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&JT=${JT}&LT=${LT}&currentJobId=${job._id}`
 			);
 		} else {
 			router.push(
@@ -206,24 +344,46 @@ const Search = () => {
 		if (currentJobId) {
 			const job = jobs.find((job) => job._id === currentJobId);
 			setSelectedJob(job);
-		} else if (jobs.length !== 0 && JT && !currentJobId) {
+		} else if (jobs.length !== 0 && JT && !LT && !currentJobId) {
 			router.push(
 				`/jobs/search?keywords=${encodeURIComponent(
 					keywords
 				)}&JT=${JT}&currentJobId=${jobs[jobs.length - 1]._id}`
 			);
-		} else if (jobs.length === 0 && JT) {
+		} else if (jobs.length === 0 && JT && !LT) {
 			router.push(
 				`/jobs/search?keywords=${encodeURIComponent(keywords)}&JT=${JT}`
 			);
-		} else if (jobs.length !== 0 && !JT && !currentJobId) {
+		} else if (jobs.length !== 0 && !JT && LT && !currentJobId) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&LT=${LT}&currentJobId=${jobs[jobs.length - 1]._id}`
+			);
+		} else if (jobs.length === 0 && !JT && LT) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(keywords)}&LT=${LT}`
+			);
+		} else if (jobs.length !== 0 && JT && LT && !currentJobId) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&JT=${JT}&LT=${LT}&currentJobId=${jobs[jobs.length - 1]._id}`
+			);
+		} else if (jobs.length === 0 && JT && LT) {
+			router.push(
+				`/jobs/search?keywords=${encodeURIComponent(
+					keywords
+				)}&JT=${JT}&LT=${LT}`
+			);
+		} else if (jobs.length !== 0 && !JT && !LT && !currentJobId) {
 			router.push(
 				`/jobs/search?keywords=${encodeURIComponent(keywords)}&currentJobId=${
 					jobs[jobs.length - 1]._id
 				}`
 			);
 		}
-	}, [jobs, JT, currentJobId]);
+	}, [jobs, JT, LT, currentJobId]);
 
 	if (search.isLoading) return <Loading />;
 
@@ -235,12 +395,21 @@ const Search = () => {
 				</Head>
 				<div className="fixed top-[4rem] bg-base-100 border-b border-base-300 w-full z-40 shadow-lg">
 					<div className="max-w-screen-xl mx-auto flex items-center py-3 divide-x divide-base-300">
-						<div className="px-5">Jobs</div>
+						<div className="px-5 font-semibold">Jobs</div>
 						<div className="flex gap-2.5 px-3">
 							<details className="dropdown">
 								<summary
 									tabIndex={0}
-									className="flex items-center gap-1 border border-base-300 rounded-full px-3 py-1 cursor-pointer"
+									className={`flex justify-center items-center gap-1 w-28 h-9 box-border rounded-full px-3 py-1 cursor-pointer font-semibold ${
+										jobType.FT ||
+										jobType.PT ||
+										jobType.C ||
+										jobType.T ||
+										jobType.I ||
+										jobType.V
+											? "bg-green-600 text-white"
+											: "border hover:border-2 border-base-300 hover:bg-base-200 duration-150"
+									}`}
 								>
 									Job type <BsCaretDownFill />
 								</summary>
@@ -251,33 +420,33 @@ const Search = () => {
 									<div className="px-2">
 										<Checkbox
 											label="Full-time"
-											checked={jobType.fullTime}
-											onChange={() => handleCheckboxChange("fullTime")}
+											checked={jobType.FT}
+											onChange={() => handleCheckboxChange("FT")}
 										/>
 										<Checkbox
 											label="Part-time"
-											checked={jobType.partTime}
-											onChange={() => handleCheckboxChange("partTime")}
+											checked={jobType.PT}
+											onChange={() => handleCheckboxChange("PT")}
 										/>
 										<Checkbox
 											label="Contract"
-											checked={jobType.contract}
-											onChange={() => handleCheckboxChange("contract")}
+											checked={jobType.C}
+											onChange={() => handleCheckboxChange("C")}
 										/>
 										<Checkbox
 											label="Temporary"
-											checked={jobType.temporary}
-											onChange={() => handleCheckboxChange("temporary")}
+											checked={jobType.T}
+											onChange={() => handleCheckboxChange("T")}
 										/>
 										<Checkbox
 											label="Internship"
-											checked={jobType.internship}
-											onChange={() => handleCheckboxChange("internship")}
+											checked={jobType.I}
+											onChange={() => handleCheckboxChange("I")}
 										/>
 										<Checkbox
 											label="Volunteer"
-											checked={jobType.volunteer}
-											onChange={() => handleCheckboxChange("volunteer")}
+											checked={jobType.V}
+											onChange={() => handleCheckboxChange("V")}
 										/>
 									</div>
 									<div className="flex justify-end border-t border-base-300 p-2">
@@ -290,32 +459,36 @@ const Search = () => {
 									</div>
 								</ul>
 							</details>
-							{/* <details className="dropdown dropdown-bottom dropdown-end flex items-center">
+							<details className="dropdown dropdown-bottom dropdown-end flex items-center">
 								<summary
 									tabIndex={0}
-									className="flex items-center gap-1 border border-base-300 rounded-full px-3 py-1 cursor-pointer"
+									className={`flex justify-center items-center gap-1 w-40 h-9 box-border rounded-full px-3 py-1 cursor-pointer font-semibold ${
+										locationType.OS || locationType.R || locationType.H
+											? "bg-green-600 text-white"
+											: "border hover:border-2 border-base-300 hover:bg-base-200 duration-150"
+									}`}
 								>
 									On-site/remote <BsCaretDownFill />
 								</summary>
 								<ul
 									tabIndex={0}
-									className="dropdown-content menu shadow bg-base-100 rounded-box w-52 border border-base-300"
+									className="dropdown-content menu rounded-lg shadow bg-base-100 w-52 border border-base-300"
 								>
 									<div className="px-2">
 										<Checkbox
 											label="On-site"
-											checked={locationType.onSite}
-											onChange={() => handleCheckboxChange("onSite")}
+											checked={locationType.OS}
+											onChange={() => handleCheckboxChange("OS")}
 										/>
 										<Checkbox
 											label="Remote"
-											checked={locationType.remote}
-											onChange={() => handleCheckboxChange("remote")}
+											checked={locationType.R}
+											onChange={() => handleCheckboxChange("R")}
 										/>
 										<Checkbox
 											label="Hybrid"
-											checked={locationType.hybrid}
-											onChange={() => handleCheckboxChange("hybrid")}
+											checked={locationType.H}
+											onChange={() => handleCheckboxChange("H")}
 										/>
 									</div>
 									<div className="flex justify-end border-t border-base-300 p-2">
@@ -327,7 +500,7 @@ const Search = () => {
 										</button>
 									</div>
 								</ul>
-							</details> */}
+							</details>
 						</div>
 					</div>
 				</div>
