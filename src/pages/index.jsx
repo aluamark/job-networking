98e8d1845dfa557646585f706c1e2d8f9cad78e0
aluamark <aluamark@gmail.com>
2023-08-07 20:48:07 +0800

    -removed h-screen in home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,38 +15,36 @@ export default function Home() {
 
 	if (status === "unauthenticated") {
 		return (
-			<div className="min-h-screen h-screen">
-				<div className="h-full max-w-screen-sm mx-auto w-full flex flex-col md:justify-center md:items-center px-5 py-5">
-					<div className="flex flex-col gap-5 md:gap-10">
-						<h1 className="text-4xl md:text-5xl font-bold">
-							Employ<span className="text-blue-600">X</span>
-						</h1>
-						<div className="flex flex-col gap-3">
-							<h2 className="text-xl font-semibold">Explore jobs</h2>
-							<SearchFilter />
-						</div>
-
-						<p>
-							Unlock endless career opportunities with our innovative job
-							posting website. Whether you&apos;re looking for your dream job or
-							trying to find the perfect candidate, our platform has everything
-							you need to succeed.
-						</p>
-						<p>
-							Find your dream job or the perfect candidate with our innovative
-							job posting website. Join now!
-						</p>
-						<div className="flex justify-end gap-3 items-center w-full bg-gradient-to-l from-blue-600 rounded-r-lg">
-							<Link href="/signup" className="link link-hover text-white">
-								Sign up
-							</Link>
-							<button onClick={() => signIn()} className="btn">
-								Login
-							</button>
-						</div>
+			<div className="min-h-screen max-w-screen-sm mx-auto w-full flex flex-col md:justify-center md:items-center px-5 py-5">
+				<div className="flex flex-col gap-5 md:gap-10">
+					<h1 className="text-4xl md:text-5xl font-bold">
+						Employ<span className="text-blue-600">X</span>
+					</h1>
+					<div className="flex flex-col gap-3">
+						<h2 className="text-xl font-semibold">Explore jobs</h2>
+						<SearchFilter />
 					</div>
-					<Footer />
+
+					<p>
+						Unlock endless career opportunities with our innovative job posting
+						website. Whether you&apos;re looking for your dream job or trying to
+						find the perfect candidate, our platform has everything you need to
+						succeed.
+					</p>
+					<p>
+						Find your dream job or the perfect candidate with our innovative job
+						posting website. Join now!
+					</p>
+					<div className="flex justify-end gap-3 items-center w-full bg-gradient-to-l from-blue-600 rounded-r-lg">
+						<Link href="/signup" className="link link-hover text-white">
+							Sign up
+						</Link>
+						<button onClick={() => signIn()} className="btn">
+							Login
+						</button>
+					</div>
 				</div>
+				<Footer />
 			</div>
 		);
 	}
